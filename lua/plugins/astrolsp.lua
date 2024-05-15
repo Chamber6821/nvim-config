254235f46367eb66e3c478dd8bb648ab7115ba2c
Chamber6821 <vania.osinin@yandex.ru>
2024-05-15 20:44:04 +0300

    feat: use jdtls formatter for Java

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -6,5 +6,13 @@ return {
     config = {
       clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
+    formatting = {
+      filter = function(client)
+        if (vim.bo.filetype == "java") then
+          return client.name == "jdtls"
+        end
+        return true
+      end
+    },
   },
 }
