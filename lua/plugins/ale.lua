aaf1fdf5403fed4a6760e2992d3694d6418a4109
Chamber6821 <vania.osinin@yandex.ru>
2025-06-07 14:31:06 +0300

    Use eslint_d instead of slow eslint

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,20 +1,26 @@
+vim.env.ESLINT_D_PPID = vim.fn.getpid()
+vim.g.ale_javascript_eslint_executable = 'eslint_d'
+vim.g.ale_javascript_eslint_use_global = 1
 return {
   'dense-analysis/ale',
   config = function()
     local g = vim.g
+    local eslint = 'eslint'
     g.ale_lint_on_save = true
     g.ale_linters = {
-      javascript = {'eslint'},
-      javascriptreact = {'eslint'},
-      typescript = {'eslint'},
-      typescriptreact = {'eslint'},
+      javascript = {eslint},
+      javascriptreact = {eslint},
+      typescript = {eslint},
+      typescriptreact = {eslint},
+      css = {eslint},
     }
     g.ale_fix_on_save = true
     g.ale_fixers = {
-      javascript = {'eslint'},
-      javascriptreact = {'eslint'},
-      typescript = {'eslint'},
-      typescriptreact = {'eslint'},
+      javascript = {eslint},
+      javascriptreact = {eslint},
+      typescript = {eslint},
+      typescriptreact = {eslint},
+      css = {eslint},
    }
   end
 }
