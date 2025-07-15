71f28828d680bce4c5db8a7ed3532904a8edb67f
Chamber6821 <vania.osinin@yandex.ru>
2025-07-16 02:05:36 +0300

    Replace ale with configured conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      css = { "prettierd" },
+    },
+    format_after_save = {
+      lsp_format = "fallback",
+    },
+  },
+}
